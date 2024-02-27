ec5f6c8f63b6461d48ff69c33e7e03374646bb5b
Jaeyong Park <72537190+nostaljic@users.noreply.github.com>
2024-02-27 09:49:02 +0900

    chore: DDP

--- Mixtral&Bx2.py
+++ Mixtral&Bx2.py
@@ -1,26 +1,31 @@
+import torch
+import torch.distributed as dist
+from torch.nn.parallel import DistributedDataParallel as DDP
+from transformers import AutoModelForCausalLM, AutoTokenizer
+import torch.multiprocessing as mp
+import argparse
+
 def setup(rank, world_size):
-    os.environ['MASTER_ADDR'] = 'localhost'
-    os.environ['MASTER_PORT'] = '12355'
-    dist.init_process_group("nccl", rank=rank, world_size=world_size)
+    dist.init_process_group(backend='nccl', init_method='tcp://127.0.0.1:23456', world_size=world_size, rank=rank)
 
 def cleanup():
     dist.destroy_process_group()
 
-def train(rank, world_size):
-    setup(rank, world_size)
-    
-    # 모델과 토크나이저를 로드합니다.
+def train(rank, ngpus_per_node, args):
+    setup(rank, args.world_size)
+
+    # 모델과 토크나이저 로드
     repo = "MarkrAI/RAG-KO-Mixtral-7Bx2-v2.1"
     tokenizer = AutoTokenizer.from_pretrained(repo)
     model = AutoModelForCausalLM.from_pretrained(repo, return_dict=True, torch_dtype=torch.float16)
-    model.to(rank)
+    model.cuda(rank)
     model = DDP(model, device_ids=[rank])
-    
-    # 입력을 준비합니다.
+
+    # 입력 준비
     prompt = '''주어진 정보를 기반으로 질문에 답하세요. 답을 모른다면 답을 지어내지 말고 그냥 모른다고 말하세요. 정보: 질문: 호모 에렉투스가 처음으로 언어를 사용하기 시작한 정확한 시점은 언제인가요? 답변: '''
     inputs = tokenizer(prompt, return_tensors="pt").to(rank)
-    
-    # 모델을 실행합니다.
+
+    # 모델 실행
     with torch.no_grad():
         generated_ids = model.module.generate(
             **inputs,
@@ -33,10 +38,20 @@ def train(rank, world_size):
         )
     
     outputs = tokenizer.batch_decode(generated_ids, skip_special_tokens=True)
-    print(outputs[0])
-    
+    if rank == 0:  # 메인 프로세스에서만 결과 출력
+        print(outputs[0])
+
     cleanup()
 
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--num_epochs', default=10, type=int)
+    parser.add_argument('--world_size', default=1, type=int)
+    args = parser.parse_args()
+
+    ngpus_per_node = torch.cuda.device_count()
+    args.world_size = ngpus_per_node * args.world_size
+    mp.spawn(train, nprocs=ngpus_per_node, args=(ngpus_per_node, args))
+
 if __name__ == "__main__":
-    world_size = torch.cuda.device_count()  # 사용 가능한 GPU 수
-    torch.multiprocessing.spawn(train, args=(world_size,), nprocs=world_size, join=True)
+    main()
